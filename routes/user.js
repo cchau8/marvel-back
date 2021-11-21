4029e285c6d6a8f2acbb1c6420ddd4a5617063e3
Cedric Chau <cedricchau87@gmail.com>
2021-11-21 18:32:51 +0100

    changed folder name

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
 const uid2 = require("uid2");
-const User = require("../models/User");
+const User = require("../Models/User");
 const generateHash = (password, salt) => {
 	return SHA256(password + salt).toString(encBase64);
 };
