982c3be31d763d9a6615ded54158c40212be8fa0
Cedric Chau <cedricchau87@gmail.com>
2021-11-21 18:21:44 +0100

    added port

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ app.all("*", (req, res) => {
 });
 
 // lancement du serveur sur le port 4000
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
 	console.log("Server has started");
 });
